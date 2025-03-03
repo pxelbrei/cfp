a7e5bd776320c028ca4c09ac12b110c09b6ae849
p.xelbrei <79312859+pxelbrei@users.noreply.github.com>
2025-03-03 12:28:27 +0100

    Update cfp.py

--- cfp.py
+++ cfp.py
@@ -3,40 +3,6 @@ import logging
 import random
 import pwnagotchi.plugins as plugins
 
-# Add new faces
-CUSTOM_FACES = {
-    'cool': '(^_^)',
-    'happy': '(^-^)',
-    'angry': '(>_<)',
-    'bored': '(-_-)',
-    'excited': '(o_O)',
-    'smart': '(=_=)',
-    'friend': '(^o^)',
-    'shocked': '(O_O)',
-    'thinking': '(-.-)',
-    'sleepy': '(-.-)Zzz',
-    'evil': '>:-)',
-    'confused': '(@_@)',
-    'surprised': '(O.O)',
-    'laughing': '(^o^)',
-    'crying': '(T_T)',
-    'determined': '(>_<)>',
-    'sneaky': '(-.-)',
-    'love': '(❤‿❤)',
-    'nerd': '(⌐■_■)',
-    'hero': '(ಠ_ಠ)',
-    'pirate': '(-_-)=b',
-    'robot': '[¬º-°]¬',
-    'ghost': '(o_o)',
-    'alien': '(👽)',
-    'ninja': '(ᗒᗣᗕ)',
-    'king': '(♚)',
-    'queen': '(♛)',
-    'joker': '(🃏)',
-    'zombie': '(☠)',
-    'monster': '(༎ຶ෴༎ຶ)',
-}
-
 # Add new phrases
 CUSTOM_PHRASES = [
     "Hack the planet!",
@@ -77,8 +43,8 @@ class CFP(plugins.Plugin):
     __version__ = '1.0.0'
     __license__ = 'GPL3'
     __description__ = (
-        'A plugin that adds custom faces and phrases. '
-        'Displays random faces and phrases based on configurable probabilities. '
+        'A plugin that adds custom phrases. '
+        'Displays random phrases based on configurable probabilities. '
         'Perfect for giving your Pwnagotchi more personality!'
     )
 
@@ -93,18 +59,8 @@ class CFP(plugins.Plugin):
         except Exception as e:
             logging.error(f"[cfp] Error in on_loaded: {e}")
 
-    def on_ui_setup(self, ui):
-        """Adds custom faces to the UI."""
-        try:
-            logging.info("[cfp] on_ui_setup called")
-            for face_name, face_char in CUSTOM_FACES.items():
-                ui.set_face(face_name.upper(), face_char)
-            logging.info("[cfp] Custom faces added to UI")
-        except Exception as e:
-            logging.error(f"[cfp] Error in on_ui_setup: {e}")
-
     def on_ui_update(self, ui):
-        """Displays random phrases and faces based on configurable probabilities."""
+        """Displays random phrases based on configurable probabilities."""
         try:
             logging.info("[cfp] on_ui_update called")
             # Show random phrases
@@ -113,13 +69,6 @@ class CFP(plugins.Plugin):
                 random_phrase = random.choice(CUSTOM_PHRASES)
                 ui.set('status', random_phrase)
                 logging.info(f"[cfp] Displayed phrase: {random_phrase}")
-
-            # Show random faces
-            face_chance = int(self.options.get('face_chance', 5))  # Default: 5%
-            if random.randint(0, 100) < face_chance:
-                random_face = random.choice(list(CUSTOM_FACES.keys()))
-                ui.set_face(random_face.upper())
-                logging.info(f"[cfp] Displayed random face: {random_face}")
         except Exception as e:
             logging.error(f"[cfp] Error in on_ui_update: {e}")
 
